56b05d30753ada78a7cee2f46b5ddfe9c2c1f65d
小卒 <chaoticjoy@gmail.com>
2020-06-27 09:33:44 +0800

    添加surge插件使用说明文件

--- packageInfo.sh
+++ packageInfo.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-perl -p -i -e "s/插件名字/电话助手iOS13破解版/g"  ./packageInfo/template/cydia副本
-perl -p -i -e "s/插件的描述文字/在https:\/\/chaoticjoy.github.io\/repo\/other\/%E7%94%B5%E8%AF%9D%E5%8A%A9%E6%89%8B.txt查看授权码/g"  ./packageInfo/template/cydia副本
+perl -p -i -e "s/插件名字/surge4.3.0 破解/g"  ./packageInfo/template/cydia副本
+perl -p -i -e "s/插件的描述文字/需要配合特定版本的APP使用，已经将能够支持的最新版本ipa上传到http:\/\/chaoticjoy.github.io\/repo\/ipa\/Surge_4.3.0_CrackerXI.zip 。/g"  ./packageInfo/template/cydia副本
 #支持系统
 #perl -p -i -e "s/IOS/IOS:13.5/g"  ./packageInfo/template/cydia副本
 
 #截图
 #perl -p -i -e "s/cydia7.png/taobaoyouhuiquan.png/g"   ./packageInfo/template/cydia副本
 
-mv ./packageInfo/template/cydia副本 ./packageInfo/com.xybp888.CallAssist13
+mv ./packageInfo/template/cydia副本 ./packageInfo/com.laxus.surge4
 
 cp ./packageInfo/template/cydia样本 ./packageInfo/template/cydia副本
